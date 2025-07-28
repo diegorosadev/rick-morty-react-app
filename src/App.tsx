4d4702a84eea642a82938be123beb3b05682950b
diegorosadev <seunovoeemail@gmail.com>
2025-07-28 17:08:48 -0300

    feat: add routes to Home and Favorites

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Home } from './pages/Home'
+import Favorites from './pages/Favorites'
+import { AppBar, Toolbar, Typography, Button } from '@mui/material'
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>Rick and Morty</Typography>
+          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button color="inherit" component={Link} to="/favorites">Favoritos</Button>
+        </Toolbar>
+      </AppBar>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
