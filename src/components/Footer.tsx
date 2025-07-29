afc92752c27c605077e323addcece5417eda91e6
diegorosadev <seunovoeemail@gmail.com>
2025-07-29 14:48:43 -0300

    change image path

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,12 +28,12 @@ export const Footer = () => {
         <Stack direction="row" spacing={3} justifyContent="center" sx={{ mt: 1 }}>
             <Stack alignItems="center">
                 <Typography variant="caption" color="gray">Deployed on Vercel</Typography>
-                <img src="public\img\Vercel_logo_2025.svg" alt="Vercel" height={20} />
+                <img src="\img\Vercel_logo_2025.svg" alt="Vercel" height={20} />
             </Stack>
 
             <Stack alignItems="center">
                 <Typography variant="caption" color="gray">Built with React</Typography>
-                <img src="public\img\React_logo_wordmark.png" alt="React" height={20} />
+                <img src="\img\React_logo_wordmark.png" alt="React" height={20} />
             </Stack>
         </Stack>
 
